97edb021455b1de805e218ff816f81d4855c0ddb
Chris Poirier <chris@courage-my-friend.org>
2015-04-12 05:58:58 -0400

    Test refactoring.

--- systems/core/tests/TestsFor_SqlParameterSet.php
+++ systems/core/tests/TestsFor_SqlParameterSet.php
@@ -4,11 +4,9 @@
   {
     function test_scalar_comparisons( $tester )
     {
-      // we use irregular spacing to ensure the regex works
-      
       $parameters = array("string" => "value");
       
-      $tests = array
+      $tests = array                       // we use irregular spacing to ensure the regex works
       (
              "field   =   {string}"
           => "field   =   'value'"
@@ -33,22 +31,15 @@
              
       );
       
-      foreach( $tests as $given => $expect )
-      {
-        $expanded    = SqlParameterSet::expand_parameters($given, $parameters);
-        $description = sprintf("%s BECOMES %s", $given, $expect);
-        $tester->record($description, $expanded == $expect, $expanded);
-      }
+      $this->run_parameter_expansion_tests($tester, $tests, $parameters);
     }
     
         
     function test_null_comparisons( $tester )
     {
-      // we use irregular spacing to ensure the regex works
-
       $parameters = array("null" => null);
       
-      $tests = array
+      $tests = array                       // we use irregular spacing to ensure the regex works
       (
              "field   =   {null}"
           => "field   is null"
@@ -73,59 +64,62 @@
              
       );
 
-
-      foreach( $tests as $given => $expect )
-      {
-        $expanded    = SqlParameterSet::expand_parameters($given, $parameters);
-        $description = sprintf("%s BECOMES %s", $given, $expect);
-        $tester->record($description, $expanded == $expect, $expanded);
-      }
+      $this->run_parameter_expansion_tests($tester, $tests, $parameters);
     }
     
     
     function test_vector_comparisons( $tester )
-    {
-      // we use irregular spacing to ensure the regex works
-      
+    {      
       $parameters = array("list" => array(1, 2, 3), "empty_list" => array(), "null" => null);
       
-      $tests = array
+      $tests = array                       // we use irregular spacing to ensure the regex works
       (
              "field   in  {list}"
           => "field   in (1, 2, 3)"
       ,   
+             "field   =   {list}"     
+          => "field   in (1, 2, 3)"        
+      ,       
+             "field   !=   {list}"     
+          => "field   not in (1, 2, 3)"        
+      ,       
              "field   in  {empty_list}"
           => "field   is null"    
+      ,   
+             "field   =   {empty_list}"
+          => "field   is null"        
+      ,   
+             "field   !=   {empty_list}"
+          => "field   is not null"        
       ,   
              "field   in  {null}"
           => "field   is null"    
       ,   
-             "field   =   {list}"     
-          => "field   in (1, 2, 3)"        
-      ,       
-             "field   =   {empty_list}"
-          => "field   is null"        
-      ,   
              "field   =   {null}"     
           => "field   is null"        
       ,  
-             "field   !=   {list}"     
-          => "field   not in (1, 2, 3)"        
-      ,       
-             "field   !=   {empty_list}"
-          => "field   is not null"        
-      ,   
              "field   !=   {null}"     
           => "field   is not null"        
       );
-      
-      
+        
+      $this->run_parameter_expansion_tests($tester, $tests, $parameters);
+    }
+     
+     
+     
+     
+     
+     
+     
+     
+     
+    function run_parameter_expansion_tests( $tester, $tests, $parameters )
+    {
       foreach( $tests as $given => $expect )
       {
         $expanded    = SqlParameterSet::expand_parameters($given, $parameters);
         $description = sprintf("%s BECOMES %s", $given, $expect);
         $tester->record($description, $expanded == $expect, $expanded);
       }
-    }
-      
+    } 
   }
